506a0819f08a389d5ccda2254ddbef56e36cefee
blaise2s <blaise.schaeffer@gmail.com>
2021-01-28 20:40:11 -0500

    feat(PW-STRENGTH): Finanizes a solution to pw srength detector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState('');
+  const [isStrong, setIsStrong] = useState(false);
+  const requirements = {
+    'At least one lowercase letter is required': /[a-z]/,
+    'At least one uppercase letter is required': /[A-Z]/,
+    'At least one number is required': /\d/,
+    'At least one of symbols is required (!@#$%^&*)': /[!@#$%^&*]/,
+  } 
+
+  const meetsRequirement = (secret, regexp) => {
+    return secret.search(regexp) > -1
+  }
+
+  const handleChange = (event) => {
+    const pswd = event.target.value
+    setPassword(pswd);
+
+    if (pswd.length > 5) {
+      Object.values(requirements).forEach((requirement) => {
+        meetsRequirement(pswd, requirement) ? setIsStrong(true) : setIsStrong(false)
+      })
+    } else {
+      setIsStrong(false)
+    }
+  }
+
+  const missingStrengths = () => {
+    const missing = []
+    for (const [message, requirement] of Object.entries(requirements)) {
+      if (!meetsRequirement(password, requirement)) missing.push(message)
+    }
+    if (password.length < 6) missing.push('Password must be at least 6 characters long')
+    return missing
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="password" value={password} onChange={handleChange} />
+      <p>Your password is {isStrong ? 'strong' : 'weak'}!</p>
+      <ul>
+        {missingStrengths().map((missingStrength) => {
+          return <li key={missingStrength}>{missingStrength}</li>
+        })}
+      </ul>
     </div>
   );
 }
